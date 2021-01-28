46c7200bf82098836fb4024a8be12050d41c3995
ManishShergill24 <legends2420@gmail.com>
2021-01-28 22:10:55 +0530

    8th

---
+++ Classes/upgrade.js
@@ -0,0 +1,18 @@
+class upgrade{
+constructor(){
+
+this.upgradeBulletsMissile = createButton("Upgrade");
+this.upgradeShipIntegrety = createButton("Upgrade");
+this.upgradeRepairTime = createButton("Upgrade");
+this.upgradeShootTimer = createButton("Upgrade");
+
+
+}
+display(){
+
+
+
+
+
+}
+}
